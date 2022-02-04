ed28bec21a421fc51f42dbd28951ef6acab0cc5d
bhargavbhatiya <bhatiyabhargav.bb@gmail.com>
2022-02-05 03:23:47 +0530

    Movie upload & streaming done

--- controllers/movieCtrl.js
+++ controllers/movieCtrl.js
@@ -1,4 +1,4 @@
-const  MoviesDBC = require("../models/movieModel");
+const Moviesdbcs = require("../models/movieModel");
 
 const movieCtrl = {
 	add_movie_details: async (req, res) => {
@@ -6,36 +6,46 @@ const movieCtrl = {
 		console.log(req.body);
 		try {
 			const {
+				movie_id,
 				title,
-				description,
-				year,
-				duration,
-				genre,
-				rating,
-				imgUrl,
+				overview,
+				genres,
+				keywords,
 				cast,
-				language,
-				country,
-				imdb_id,
+				crew,
+				popularity,
+				production_companies,
+				runtime,
+				tagline,
+				vote_average,
+				vote_count,
+				release_date,
+				original_language,
+				videoUrl,
 			} = req.body;
 
-			const newMovie = new Movies({
-				title,
-				description,
-				year,
-				duration,
-				genre,
-				rating,
-				imgUrl,
-				cast,
-				language,
-				country,
-				imdb_id,
-				uploadedBy: req.user.id,
-			}
-			//collection: "movies",
+			const newMovie = new Moviesdbcs(
+				{
+					movie_id,
+					title,
+					overview,
+					genres,
+					keywords,
+					cast,
+					crew,
+					popularity,
+					production_companies,
+					runtime,
+					tagline,
+					vote_average,
+					vote_count,
+					release_date,
+					original_language,
+					videoUrl,
+					uploadedBy: req.user.id,
+				}
+				// { collection: "moviesdbcs" }
 			);
-
 			await newMovie.save();
 
 			res.json({ msg: "Details Added Successfully!" });
@@ -49,18 +59,14 @@ const movieCtrl = {
 		// console.log(req.user);
 		//console.log(req.body);
 		try {
-		
-			const movies = await MoviesDBC.find({}).limit(20);
-				//console.log("first"+movies);
+			const movies = await Moviesdbcs.find({}).limit(20);
+			//console.log("first"+movies);
 			const allmovies = movies.map((movie) => {
 				console.log(movie.movie_id);
 				return movie;
 			});
 
-		
-
-			res.json({ msg: "movie find successfully",  
-			movies:allmovies });
+			res.json({ msg: "movie find successfully", movies: allmovies });
 		} catch (err) {
 			console.log(err);
 			return res.status(500).json({ msg: err.message });
@@ -69,25 +75,23 @@ const movieCtrl = {
 
 	get_movie: async (req, res) => {
 		console.log("inside get_movie using id");
-		// console.log(req.user);
-		//console.log(req.body);
-		try {
+		console.log(req.user);
 		console.log(req.params.id);
+		try {
+			console.log(req.params.id);
 
-			const movie = await MoviesDBC.find({title: new RegExp(`^${req.params.id}$`, 'i')});
-				//console.log("first"+movies);
-			if(movie.length==0)
-			{
+			const movie = await Moviesdbcs.find({
+				// title: new RegExp(`^${req.params.id}$`, "i"),
+				movie_id: req.params.id,
+			});
+			// console.log("first" + movies);
+			if (movie.length == 0) {
 				return res.status(404).json({ msg: "movie not found" });
 			}
 
-
-		
-
-			res.json({ msg: "movie find successfully",  
-			movie:movie });
+			res.json({ msg: "movie find successfully", movie: movie });
 		} catch (err) {
-			console.log(err);
+			console.log(err.message);
 			return res.status(500).json({ msg: err.message });
 		}
 	},
--- models/movieModel.js
+++ models/movieModel.js
@@ -1,86 +1,82 @@
 const mongoose = require("mongoose");
 const double = require("@mongoosejs/double");
-const movieSchema = new mongoose.Schema(
-	{
-
-		movie_id: {
-			type: Number,
-			required: [true, "Please enter movie id!"],
-			unique: true,
-		},
-		title: {
-			type: String,
-			//required: [true, "Please enter movie Title!"],
-			
-		},
-		overview: {
-			type: String,
-			//required: [true, "Please enter movie description!"],
-		},
-		genres: {
-			type: String,
-			//required: [true, "Please enter movie genres!"],
-		},
-
-	    keywords: {
-			type: String,
-			//required: [true, "Please enter movie keywords!"],
-		},
-
-	       cast: {
-			type: String,
-			//required: [true, "Please enter movie cast!"]
-		},
-
-		crew: {
-			type: String,
-			//required: [true, "Please enter movie crew!"]
-		},
-
-		popularity: {
-			type: double,
-			//required: [true, "Please enter movie duration!"],
-		},
-	
-		production_companies: {
-			type: String,
-			//required: [true, "Please enter movie production companies!"],
-		},
-		runtime: {
-			type: double,
-			//required: [true, "Please enter movie duration!"],
-		},
-
-		tagline: {
-			type: String,
-			//required: [true, "Please enter movie tagline!"],
-		},
-
-		vote_average: {
-			type: double,
-			//required: [true, "Please enter movie vote average!"],
-		},
-
-		vote_count: {
-			type: Number,
-			//required: [true, "Please enter movie vote count!"],
-		},
-
-		release_date: {
-			type: String,
-			//required: [true, "Please enter movie release date!"],
-		},
-
-		original_language: {
-			type: String,
-			//required: [true, "Please enter movie original language!"],
-		},
-		
-		videoUrl: {
-			type: String,
-			default: "",
-		}
+const movieSchema = new mongoose.Schema({
+	movie_id: {
+		type: Number,
+		required: [true, "Please enter movie id!"],
+		unique: true,
 	},
-);
+	title: {
+		type: String,
+		//required: [true, "Please enter movie Title!"],
+	},
+	overview: {
+		type: String,
+		//required: [true, "Please enter movie description!"],
+	},
+	genres: {
+		type: String,
+		//required: [true, "Please enter movie genres!"],
+	},
 
-module.exports = mongoose.model("moviesDBC", movieSchema, "moviesDBC");
+	keywords: {
+		type: String,
+		//required: [true, "Please enter movie keywords!"],
+	},
+
+	cast: {
+		type: String,
+		//required: [true, "Please enter movie cast!"]
+	},
+
+	crew: {
+		type: String,
+		//required: [true, "Please enter movie crew!"]
+	},
+
+	popularity: {
+		type: String,
+		//required: [true, "Please enter movie duration!"],
+	},
+
+	production_companies: {
+		type: String,
+		//required: [true, "Please enter movie production companies!"],
+	},
+	runtime: {
+		type: String,
+		//required: [true, "Please enter movie duration!"],
+	},
+
+	tagline: {
+		type: String,
+		//required: [true, "Please enter movie tagline!"],
+	},
+
+	vote_average: {
+		type: String,
+		//required: [true, "Please enter movie vote average!"],
+	},
+
+	vote_count: {
+		type: Number,
+		//required: [true, "Please enter movie vote count!"],
+	},
+
+	release_date: {
+		type: String,
+		//required: [true, "Please enter movie release date!"],
+	},
+
+	original_language: {
+		type: String,
+		//required: [true, "Please enter movie original language!"],
+	},
+
+	videoUrl: {
+		type: String,
+		default: "",
+	},
+});
+
+module.exports = mongoose.model("moviesdbcs", movieSchema, "moviesdbcs");
